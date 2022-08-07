0529ca69d26aae88bf00359ef1fd13760f57924b
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-08-07 22:48:46 +0530

    added chakra and basic setup done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,45 @@
-import '../styles/globals.css'
+import { AuthProvider } from "../lib/auth"
+import { ChakraProvider, CSSReset } from '@chakra-ui/react'
+import { myTheme } from "../styles/theme"
+import { extendTheme } from '@chakra-ui/react'
+import Head from 'next/head'
+import { Global, css } from '@emotion/react'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const theme = extendTheme({ myTheme })
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  )
+}
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import styles from '../styles/Home.module.css'
+import { Button, Heading, Text, Code } from '@chakra-ui/react'
+
+import { useAuth } from '@/lib/auth'
 
 export default function Home() {
+  const auth = useAuth()
   return (
-    <div className={styles.container}>
-      Hello
+    <div>
+      <Heading>Feedback Helper</Heading>
+      {!auth?.user && (
+        <Button onClick={() => auth.signinWithGitHub()}>Sign in with github</Button>
+      )}
+      <Text>
+        Current user: <Code>{auth.user ? auth.user.email : 'None'}</Code>
+      </Text>
+      {auth?.user?.name}
+      {auth?.user && (
+        <Button onClick={() => auth.signOut()}>Sign out</Button>
+      )}
     </div>
   )
 }
